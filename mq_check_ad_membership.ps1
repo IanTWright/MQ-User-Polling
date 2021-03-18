649c6aa6ddc0410e1ede49921acb29e15d7cd89e
Ian Wright <ian@iantwright.com>
2021-03-18 11:03:16 -0400

    Displays group memberships
    Script indexes the object and appends group memberships to the user entry.

--- mq_check_ad_membership.ps1
+++ mq_check_ad_membership.ps1
@@ -1,20 +1,27 @@
-$AdGroups = Get-ADGroup -filter {name -like '*-MQ-*'} | Select-Object Name
+$SearchFilter = Read-Host "Active Directory Group Filter"
+$ExportPath = Read-Host "Please provide export path"
+
+
+$AdGroups = Get-ADGroup -filter {name -like $SearchFilter} | Select-Object name
 
 $UserArray = @()
+$UserArray += New-Object PSObject -Property @{User="";Group=""}
 ForEach ($g in $AdGroups) {
-    Write-Host -ForegroundColor Cyan "Checking AD group $($g.Name)."
-    $Users = Get-ADGroupMember -Identity $g.Name | Select-Object name
+    Write-Host -ForegroundColor Cyan "Checking AD group $($g.name)."
+    $Users = Get-ADGroupMember -Identity $g.name | Select-Object name
 
     ForEach ($u in $Users) {
-    If(!($UserArray -contains $u.name)){
-        $UserArray += $u.name
-        Write-Host -ForegroundColor Green "Adding $($u.name) to UserArray."
+        $Index = [Array]::FindIndex($UserArray, [System.Predicate[PSCustomObject]]{$args[0].User -eq $u.name})
+
+        if ($Index -ge 0) {
+            Write-Host -ForegroundColor Yellow "Adding $($g.name) to $($u.name)"
+            $UserArray[$Index].Group += ", $($g.name)"
         }
-    Elseif ($UserArray -contains $u.name) {
-        Write-Host -ForegroundColor Yellow "UserArray already contains $($u.name). Skipping..."
-        continue
+        else {
+            Write-Host -ForegroundColor Green "Adding $($u.name) to table"
+            $UserArray += New-Object PSObject -Property @{User=$u.name;Group=$g.name}
         }
     }
 }
-
-$UserArray | Out-File C:\Users\itw2a0\Desktop\DataArk_Users.csv
+$UserArray | Export-Csv -NoTypeInformation -Path $ExportPath
+Write-Host "Exported CSV to $ExportPath."
